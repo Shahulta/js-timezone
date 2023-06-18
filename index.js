5cdc13e0f8c1ebf83744b66b46624d58a0df7efc
Shahulta <131171791+Shahulta@users.noreply.github.com>
2023-06-18 20:55:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
-
-function increment() {
-    count += 1
-    countEl.textContent = count 
-}
-
-function save() {
-    let countStr = count + " - "
-
-    saveEl.textContent += countStr
-    console.log(count)
+function timeZone() {
+    document.getElementById("time").innerHTML = Date() 
 }
